ddc7ccbf09360ce274fbc6b8a52e49073cb3d3d1
João Carlos <joaoarcantos@outlook.com>
2025-04-15 11:42:50 -0300

    ...

--- api_football.py
+++ api_football.py
@@ -12,11 +12,13 @@ headers = {
     "x-apisports-key": API_KEY
 }
 
+# Retorna todas as ligas disponíveis na API
 def get_leagues():
     url = f"{BASE_URL}/leagues"
     res = requests.get(url, headers=headers)
     return res.json()["response"]
 
+# Retorna as temporadas disponíveis para uma liga específica
 def get_seasons_for_league(league_id):
     url = f"{BASE_URL}/leagues?id={league_id}"
     res = requests.get(url, headers=headers)
@@ -25,6 +27,7 @@ def get_seasons_for_league(league_id):
         return data[0]["seasons"]
     return []
 
+# Retorna as partidas (fixtures) de uma liga em uma temporada específica
 def get_fixtures(league_id, season):
     url = f"{BASE_URL}/fixtures?league={league_id}&season={season}"
     res = requests.get(url, headers=headers)
@@ -33,6 +36,7 @@ def get_fixtures(league_id, season):
         return []
     return res.json()["response"]
 
+# Retorna as partidas (fixtures) que acontecem na data atual
 def get_fixtures_today():
     today = date.today().strftime("%Y-%m-%d")
     url = f"{BASE_URL}/fixtures?date={today}"
@@ -43,6 +47,7 @@ def get_fixtures_today():
         print("Erro:", response.text)
         return []
 
+# Retorna os times de uma liga em uma temporada específica
 def get_teams_for_league(league_id, season):
     url = f"{BASE_URL}/teams?league={league_id}&season={season}"
     res = requests.get(url, headers=headers)
@@ -52,6 +57,7 @@ def get_teams_for_league(league_id, season):
         print("Erro ao buscar times:", res.text)
         return []
 
+# Retorna as estatísticas de uma partida (fixture) específica
 def get_statistics_for_fixture(fixture_id):
     url = f"{BASE_URL}/fixtures/statistics?fixture={fixture_id}"
     res = requests.get(url, headers=headers)
